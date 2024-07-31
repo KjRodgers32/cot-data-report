68d33d9d4759d6003b57901e0045a1538f83096c
Kevin <kjrodgers26@gmail.com>
2024-07-30 23:05:25 -0400

    fixed bug in queries

--- server/src/queries/CHFQueries.js
+++ server/src/queries/CHFQueries.js
@@ -1,11 +1,11 @@
 const ALL_CHF_DATA = `select * from futures_data
 where "Market and Exchange Names" = 'SWISS FRANC - CHICAGO MERCANTILE EXCHANGE' and
 "As of Date in Form YYYY-MM-DD"::date between (select max("As of Date in Form YYYY-MM-DD")
-from futures_data)::date - 360 and (select max("As of Date in Form YYYY-MM-DD") from futures_data)::date order by "As of Date in Form YYYY-MM-DD"::date  ;`;
+from futures_data)::date - 360 and (select max("As of Date in Form YYYY-MM-DD") from futures_data)::date order by "As of Date in Form YYYY-MM-DD"::date;`;
 
 const MOST_RECENT_CHF_DATA = `select * from futures_data 
 where "Market and Exchange Names" = 'SWISS FRANC - CHICAGO MERCANTILE EXCHANGE' and
  "As of Date in Form YYYY-MM-DD" = (select max("As of Date in Form YYYY-MM-DD") 	
- from futures_data) order by "As of Date in Form YYYY-MM-DD"::date ;`;
+ from futures_data) order by "As of Date in Form YYYY-MM-DD"::date;`;
 
 module.exports = { ALL_CHF_DATA, MOST_RECENT_CHF_DATA };
--- server/src/queries/USDQueries.js
+++ server/src/queries/USDQueries.js
@@ -1,11 +1,11 @@
 const ALL_USD_DATA = `select * from futures_data
 where "Market and Exchange Names" = 'USD INDEX - ICE FUTURES U.S.' and
 "As of Date in Form YYYY-MM-DD"::date between (select max("As of Date in Form YYYY-MM-DD")
-from futures_data)::date - 360 and (select max("As of Date in Form YYYY-MM-DD") from futures_data)::date order by "As of Date in Form YYYY-MM-DD"::date ;`;
+from futures_data)::date - 360 and (select max("As of Date in Form YYYY-MM-DD") from futures_data)::date order by "As of Date in Form YYYY-MM-DD"::date;`;
 
 const MOST_RECENT_USD_DATA = `select * from futures_data 
 where "Market and Exchange Names" = 'USD INDEX - ICE FUTURES U.S.' and 
 "As of Date in Form YYYY-MM-DD" = (select max("As of Date in Form YYYY-MM-DD") 	
-from futures_data) order by "As of Date in Form YYYY-MM-DD"::date ;`;
+from futures_data) order by "As of Date in Form YYYY-MM-DD"::date;`;
 
 module.exports = { ALL_USD_DATA, MOST_RECENT_USD_DATA };
